365e286d4020d92c3b0d5e5a8a302294e65c1c2e
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2025-05-11 04:36:37 +0530

    Tailwind mssg added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import  TailwindLogo  from './assets/tailwindcss.svg'
 import './App.css'
 
 function App() {
@@ -8,15 +9,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+      <div className='flex items-center justify-center'>
+        <a href="https://vite.dev" target="_blank" className='flex items-center'>
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" className='flex items-center'>
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://tailwindcss.com" target="_blank" className='flex items-center'>
+          <img src={TailwindLogo} className="logo tailwind" alt="Tailwind CSS logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TailwindCSS</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
